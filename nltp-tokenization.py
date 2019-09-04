cd6abafbcc40fb612391a9cb49d7de6daf65bc40
HyowonAn <hyowon@hanwha.com>
2019-09-04 17:07:38 +0900

    First Commit

---
+++ nltp-tokenization.py
@@ -0,0 +1,8 @@
+#%%
+import nltk
+from nltk.tokenize import word_tokenize
+#%%
+print(word_tokenize("Don't be fooled by the dark sounding name, Mr. Jone's Orphanage is as cheery as cheery goes for a pastry shop."))  
+
+
+#%%
